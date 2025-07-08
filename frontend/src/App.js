5ce5054c6c244b4492b8bff80bf5ec01febe5eb9
Narad Saud <naradsaud29@gmail.com>
2025-07-08 18:37:03 +0545

    tailwind install

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 import { AuthProvider } from './context/AuthContext';
+import './index.css';
+
 
 const App = () => {
   return (
